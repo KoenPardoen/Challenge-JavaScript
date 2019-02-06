f5613ffe116659d4a9c535d4e013ab597809297c
KoenPardoen <koenpardoen@hotmail.com>
2019-02-06 14:07:35 +0100

    styling & js navigation
    navigeren door de vragen.

--- javascript/script.js
+++ javascript/script.js
@@ -1,34 +1,63 @@
+const intro = document.getElementById("intro");
+const statements = document.getElementById("statements");
+const result = document.getElementById("result"); 
 const title = document.getElementById("title");
-const statement = document.getElementById("statements");
-var a = 0
-var pro = 0
-var contra = 0
+const statement = document.getElementById("statement");
+var a = 0;
+
+var answers = [];
 
 
+function main() {
+	intro.style.display = "block";
+	statements.style.display = "none";
+}
 
 function start() {
+	intro.style.display = "none";
+	statements.style.display = "block";
 	title.innerHTML = subjects[a].title;
 	statement.innerHTML = subjects[a].statement;
 }
 
-function agree() {
-	a++
-	pro++
-	start()
+function answer(value) {
+	if (a !== subjects.length) {
+		// agree
+		if (value === 1) {
+		answers[a] = 1;
+		a++;
+		start();
+		}
+		// disagree 
+		else if (value === 2) {
+		answers[a] = 2;
+		a++;
+		start();
+		} 
+		// none
+		else if (value === 3) {
+		answers[a] = 3;
+		a++;
+		start();
+		} 
+		// skip
+		else if (value === 3) {
+		a++;
+		start();
+		} 
+	}
 }
 
-function disagree() {
-	a++
-	contra--
-	start()
-}
 
-function none() {
-	a++
-	start()
-}
 
 function back() {
-	a --
-	start()
+		if (a===0) {
+			main();
+	} else{
+		a--;
+		start();
+	}
 }
+
+
+
